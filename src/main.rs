8460debb3e4c94bed4ef5bbe7199e6e6c61c9788
Cem Özden <mirlukadev@gmail.com>
2025-03-14 12:03:25 +0100

    Supporting pick keyword

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,24 @@ use std::fmt::Display;
 #[derive(Debug, PartialEq, Eq, Clone, Copy)]
 enum Keyword {
     Get,
+    Pick,
+    And,
 }
 
 const KEYWORDS_MAP: phf::Map<&'static str, Keyword> = phf_map! {
-    "get" => Keyword::Get
+    "get" => Keyword::Get,
+    "pick" => Keyword::Pick,
+    "and" => Keyword::And,
 };
 
-const SINGLE_CHAR_OPERATORS: &[char] = &[',', '(', ')'];
+const SINGLE_CHAR_OPERATORS: &[char] = &[',', '(', ')', '=', '>', '-'];
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq)]
 enum LexerError {
-    EmptyQuery,
+    ExhaustedInput,
     UnknownCharacter(char),
+    InvalidInteger(usize),
+    UnknownError(usize),
 }
 
 impl std::error::Error for LexerError {}
@@ -23,21 +29,37 @@ impl std::error::Error for LexerError {}
 impl Display for LexerError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            LexerError::EmptyQuery => write!(f, "Empty Marduk query"),
+            LexerError::ExhaustedInput => write!(f, "Exhausted Input"),
             LexerError::UnknownCharacter(ch) => write!(f, "Unknown character in the query: {}", ch),
+            LexerError::InvalidInteger(offset) => {
+                write!(f, "Invalid integer at position {}", offset)
+            }
+            LexerError::UnknownError(offset) => {
+                write!(f, "An unknown error occurred at position {}", offset)
+            }
         }
     }
 }
 
 #[derive(Debug, PartialEq, Eq)]
-enum Token {
-    Word(String),
+enum Operator {
     OpenParantheses,
     CloseParantheses,
     Comma,
-    Keyword(Keyword),
+    Equals,
+    BiggerThan,
 }
 
+#[derive(Debug, PartialEq, Eq)]
+enum Token {
+    Word(String),
+    Operator(Operator),
+    Keyword(Keyword),
+    String(String),
+    Integer(i32),
+}
+
+#[derive(Debug)]
 struct Lexer {
     query_str: String,
 }
@@ -45,7 +67,7 @@ struct Lexer {
 impl Lexer {
     pub fn new(query: String) -> Result<Self, LexerError> {
         if query.is_empty() {
-            return Err(LexerError::EmptyQuery);
+            return Err(LexerError::ExhaustedInput);
         }
 
         Ok(Self { query_str: query })
@@ -65,10 +87,17 @@ impl Lexer {
             }
 
             let token = match ch {
-                'a'..'z' => self.parse_word_or_keyword(ch, &chars, &mut offset)?,
-                '(' => Token::OpenParantheses,
-                ')' => Token::CloseParantheses,
-                ',' => Token::Comma,
+                'a'..'z' => self.parse_word_or_keyword(ch, &chars, &mut offset),
+                'A'..'Z' => self.parse_word_or_keyword(ch, &chars, &mut offset),
+                '0'..'9' => self.parse_integer(ch, &chars, &mut offset)?,
+                // Parse negative integers
+                '-' => self.parse_integer(ch, &chars, &mut offset)?,
+                '(' => Token::Operator(Operator::OpenParantheses),
+                ')' => Token::Operator(Operator::CloseParantheses),
+                ',' => Token::Operator(Operator::Comma),
+                '=' => Token::Operator(Operator::Equals),
+                '>' => Token::Operator(Operator::BiggerThan),
+                '"' => self.parse_string(&chars, &mut offset)?,
                 _ => return Err(LexerError::UnknownCharacter(ch)),
             };
 
@@ -78,12 +107,72 @@ impl Lexer {
         Ok(tokens)
     }
 
-    fn parse_word_or_keyword(
+    fn parse_string(&self, chars: &[char], offset: &mut usize) -> Result<Token, LexerError> {
+        let mut string_completed: bool = false;
+        let mut ch_vector = Vec::with_capacity(30);
+
+        while *offset < chars.len() {
+            let ch = chars[*offset];
+
+            if ch == '"' {
+                string_completed = true;
+                break;
+            }
+
+            ch_vector.push(ch);
+            *offset += 1;
+        }
+
+        println!("{:?}", ch_vector);
+
+        if string_completed {
+            let string: String = ch_vector.into_iter().collect();
+            *offset += 1;
+
+            Ok(Token::String(string))
+        } else {
+            Err(LexerError::ExhaustedInput)
+        }
+    }
+
+    fn parse_integer(
         &self,
         read_char: char,
         chars: &[char],
         offset: &mut usize,
     ) -> Result<Token, LexerError> {
+        let mut int_vector = Vec::with_capacity(10);
+        int_vector.push(read_char);
+
+        while *offset < chars.len() {
+            let ch = chars[*offset];
+
+            if ch == ' ' {
+                break;
+            }
+
+            if ch >= '0' && ch <= '9' {
+                int_vector.push(ch);
+            } else {
+                return Err(LexerError::InvalidInteger(*offset));
+            }
+
+            *offset += 1;
+        }
+
+        if int_vector.len() == 1 && read_char == '-' {
+            return Err(LexerError::InvalidInteger(*offset - 1));
+        }
+
+        let int_str: String = int_vector.into_iter().collect();
+
+        int_str
+            .parse::<i32>()
+            .map_err(|_| LexerError::UnknownError(*offset))
+            .map(|i| Token::Integer(i))
+    }
+
+    fn parse_word_or_keyword(&self, read_char: char, chars: &[char], offset: &mut usize) -> Token {
         let mut word_vector = Vec::with_capacity(30);
 
         word_vector.push(read_char);
@@ -106,11 +195,11 @@ impl Lexer {
         let word = String::from_iter(word_vector);
 
         let token = KEYWORDS_MAP
-            .get(word.as_str())
+            .get(word.to_lowercase().as_str())
             .map(|keyword| Token::Keyword(keyword.to_owned()))
             .unwrap_or(Token::Word(word));
 
-        Ok(token)
+        token
     }
 
     fn is_escape_character(&self, ch: char) -> bool {
@@ -128,19 +217,130 @@ mod tests {
     use super::*;
 
     #[test]
-    fn parse_get_query_with_object_name() {
-        let test_query: &str = "get process_az(name, start_time)";
+    fn empty_query_should_yield_exhausted_input_error() {
+        let test_query: &str = "";
 
-        let lexer = Lexer::new(test_query.to_string()).unwrap();
+        let lexer = Lexer::new(test_query.to_string()).unwrap_err();
+
+        assert_eq!(lexer, LexerError::ExhaustedInput);
+    }
+
+    #[test]
+    fn parse_word() {
+        let query: &str = "process";
+        let expected_tokens = vec![Token::Word("process".to_string())];
+
+        let lexer = Lexer::new(query.to_string()).unwrap();
+
+        match lexer.generate_tokens() {
+            Ok(tokens) => assert_eq!(tokens, expected_tokens),
+            Err(err) => panic!("Test failed with error: {:?}", err),
+        }
+    }
+
+    #[test]
+    fn parse_string() {
+        let query: &str = "\"marduk\"";
+        let expected_tokens = vec![Token::String("marduk".to_string())];
+
+        let lexer = Lexer::new(query.to_string()).unwrap();
+
+        match lexer.generate_tokens() {
+            Ok(tokens) => assert_eq!(tokens, expected_tokens),
+            Err(err) => panic!("Test failed with error: {:?}", err),
+        }
+    }
+
+    #[test]
+    fn fail_to_parse_invalid_integers() {
+        let query: &str = "1a0";
+
+        let lexer = Lexer::new(query.to_string()).unwrap();
+
+        let error = lexer.generate_tokens().unwrap_err();
+
+        assert_eq!(error, LexerError::InvalidInteger(1))
+    }
+
+    #[test]
+    fn fail_to_parse_single_minus_character() {
+        let query: &str = "-";
+
+        let lexer = Lexer::new(query.to_string()).unwrap();
+
+        let error = lexer.generate_tokens().unwrap_err();
+
+        assert_eq!(error, LexerError::InvalidInteger(0))
+    }
+
+    #[test]
+    fn parse_positive_integer() {
+        let query: &str = "10";
+        let expected_tokens = vec![Token::Integer(10)];
+
+        let lexer = Lexer::new(query.to_string()).unwrap();
+
+        match lexer.generate_tokens() {
+            Ok(tokens) => assert_eq!(tokens, expected_tokens),
+            Err(err) => panic!("Test failed with error: {:?}", err),
+        }
+    }
+
+    #[test]
+    fn parse_negative_integer() {
+        let query: &str = "-10";
+        let expected_tokens = vec![Token::Integer(-10)];
+
+        let lexer = Lexer::new(query.to_string()).unwrap();
+
+        match lexer.generate_tokens() {
+            Ok(tokens) => assert_eq!(tokens, expected_tokens),
+            Err(err) => panic!("Test failed with error: {:?}", err),
+        }
+    }
+
+    #[test]
+    fn parse_get_query_with_object_name() {
+        let query: &str = "get process_az(name, start_time)";
+
+        let lexer = Lexer::new(query.to_string()).unwrap();
 
         let expected_tokens = vec![
             Token::Keyword(Keyword::Get),
             Token::Word("process_az".to_string()),
-            Token::OpenParantheses,
+            Token::Operator(Operator::OpenParantheses),
             Token::Word("name".to_string()),
-            Token::Comma,
+            Token::Operator(Operator::Comma),
             Token::Word("start_time".to_string()),
-            Token::CloseParantheses,
+            Token::Operator(Operator::CloseParantheses),
+        ];
+
+        match lexer.generate_tokens() {
+            Ok(tokens) => assert_eq!(tokens, expected_tokens),
+            Err(err) => panic!("Test failed with error: {:?}", err),
+        }
+    }
+
+    #[test]
+    fn parse_get_query_with_pick_keyword() {
+        let query: &str = "get process pick name = \"marduk\" AND pid > 100 AND temp = -12";
+        let lexer = Lexer::new(query.to_string()).unwrap();
+
+        let expected_tokens = vec![
+            Token::Keyword(Keyword::Get),
+            Token::Word("process".to_string()),
+            Token::Keyword(Keyword::Pick),
+            Token::Word("name".to_string()),
+            Token::Operator(Operator::Equals),
+            Token::String("marduk".to_string()),
+            Token::Keyword(Keyword::And),
+            Token::Word("pid".to_string()),
+            Token::Operator(Operator::BiggerThan),
+            Token::Integer(100),
+            Token::Keyword(Keyword::And),
+            Token::Word("temp".to_string()),
+            Token::Operator(Operator::Equals),
+            Token::Integer(-12),
         ];
 
         match lexer.generate_tokens() {
